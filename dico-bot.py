a12a16d3700881effea6ba5dfb1eb75f937179ce
hyoseo <hyoseo837@gmail.com>
2021-03-23 22:03:54 +0100

    first commit

---
+++ dico-bot.py
@@ -0,0 +1,40 @@
+import discord
+
+
+class chatbot(discord.Client):
+    # 프로그램이 처음 실행되었을 때 초기 구성
+    async def on_ready(self):
+        # 상태 메시지 설정
+        # 종류는 3가지: Game, Streaming, CustomActivity
+        game = discord.Game("멈춰 대기")
+
+        # 계정 상태를 변경한다.
+        # 온라인 상태, game 중으로 설정
+        await client.change_presence(status=discord.Status.online, activity=game)
+
+        # 준비가 완료되면 콘솔 창에 "READY!"라고 표시
+        print("READY")
+
+    # 봇에 메시지가 오면 수행 될 액션
+    async def on_message(self, message):
+        # SENDER가 BOT일 경우 반응을 하지 않도록 한다.
+        if message.author.bot:
+            return None
+        
+        # message.content = message의 내용
+        if message.content[-3:] == "멈춰!":
+            # 현재 채널을 받아옴
+            channel = message.channel
+            # 답변 내용 구성
+            msg = message.content
+            # msg에 지정된 내용대로 메시지를 전송
+            await channel.send(msg)
+            return None
+
+
+# 프로그램이 실행되면 제일 처음으로 실행되는 함수
+if __name__ == "__main__":
+    # 객체를 생성
+    client = chatbot()
+    # TOKEN 값을 통해 로그인하고 봇을 실행
+    client.run("ODI0MDA0Nzk2MDY3MDg2Mzg3.YFpERA.2CsZGSevhfEFjakbgJK-Mbtewvw")
