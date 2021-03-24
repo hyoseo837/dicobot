d86dbe69557dcb934d96b5ae42b7ec6419e40ae7
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2021-03-24 22:33:59 +0100

    마지막

--- dico2-bot.py
+++ dico2-bot.py
@@ -43,7 +43,6 @@ class chatbot(discord.Client):
             channel = message.channel
             msg = "속이 뻥 (울컥울컥)"
             await channel.send(msg)
-            return None
         if message.content[-5:] == ('뭐냐면..'):
             channel = message.channel
             msg = "네~ 알려드렸습니다~"
@@ -54,6 +53,32 @@ class chatbot(discord.Client):
             msg = "밈밈아\n무야호!\n~멈춰!\n미안하다\n포브스 선정\nr 분의 y\n이야..\n~던가!\n뭐야~줘요\n미안하다\n엄\n~뭐냐면.."
             await channel.send(msg)
             return None
+        # if message.content == ('r 분의 y'):
+        #     channel = message.channel
+        #     msg = "사인함수~ r 분의 x 코사인 함수~"
+        #     await channel.send(msg)
+        #     return None
+        # if message.content.startswith('이야..'):
+        #     channel = message.channel
+        #     msg = "그죠..?"
+        #     await channel.send(msg)
+        #     return None
+        #     return None
+        # if message.content.startswith('뭐야') and message.content[-3:] == ('줘요'):
+        #     channel = message.channel
+        #     msg = "이래서 눈치빠른 놈들은 싫다니까"
+        #     await channel.send(msg)
+        #     return None
+        # if message.content.startswith('미안하다'):
+        #     channel = message.channel
+        #     msg = "이거 보여주려고 어그로 끌었다"
+        #     await channel.send(msg)
+        #     return None
+        # if message.content == ('엄'):
+        #     channel = message.channel
+        #     msg = "준"
+        #     await channel.send(msg)
+        #     return None
 if __name__ == "__main__":
     client = chatbot()
     client.run(os.environ["token2"])
